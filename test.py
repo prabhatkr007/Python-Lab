38752f30b62321a68bb8fab893a019c35a246cdd
prabhatkr007 <pkr.876550@gmail.com>
2023-03-14 09:56:35 +0530

    aise hee

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-stack = [1,2,3]
+class complex (object):
+    def __init__(self,real, imag = 0):
+            
+        self.real = float(real)
+        self.imag = float(imag)
+            
+    def __add__(self, other):
+        return complex(self.real + other.real, self.imag + other.imag)
+            
+    def __radd(self ,other):
+        return complex(self.real + other.real, self.imag + other.imag)
+                
+            
+c1 = complex(2,3)
+c2 = complex(4,5)
 
-stack.append(4)
-stack.append(5)
-
-print(stack)
-
-stack.pop()
-
-print(stack)
-
-print(stack.pop())
-
-print(stack)
+print((c1+4.0).real,(c1+4.0).imag)
